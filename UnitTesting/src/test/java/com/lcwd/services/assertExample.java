83d61e9a8db3a981bd0b413b56fb998d406f44e3
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-10-10 11:03:25 +0530

    Update assertExample.java

--- UnitTesting/src/test/java/com/lcwd/services/assertExample.java
+++ UnitTesting/src/test/java/com/lcwd/services/assertExample.java
@@ -1,5 +1,6 @@
 package com.lcwd.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class assertExample {
@@ -8,6 +9,11 @@ public class assertExample {
 	@Test
 	public void test1() {
 		System.out.println("Testing some assertion methods");
+		
+		byte actual = 12;
+		byte expected = 12;
+		
+		Assertions.assertEquals(expected, actual);
 		
 	}
 
